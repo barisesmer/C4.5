660d880e1b2c21755e335b38f328cf0115071439
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2019-06-16 12:29:05 +0200

    Fix issue wih discrete values, added weather example

--- c45/main.py
+++ c45/main.py
@@ -2,6 +2,15 @@
 import pdb
 from c45 import C45
 
+# Run both continuous and discrete examples
+print("[Weather data]")
+c1 = C45("../data/weather/weather.data", "../data/weather/weather.names")
+c1.fetchData()
+c1.preprocessData()
+c1.generateTree()
+c1.printTree()
+
+print("\n[Iris data]")
 c1 = C45("../data/iris/iris.data", "../data/iris/iris.names")
 c1.fetchData()
 c1.preprocessData()
